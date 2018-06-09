2ef9a55092cdfe878d85a1625da6ea0748494da2
github username <github email address>
2018-06-09 12:43:21 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |attribute, char_hash|
+    char_hash.each do |characteristic, names|
+      names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][attribute] ||= []
+        pigeon_list[name][attribute] << characteristic.to_s
+      end
+    end
+  end
+pigeon_list
 end
